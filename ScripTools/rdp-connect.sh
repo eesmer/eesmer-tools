95092a3570cf37481a88e7116ed30264a09510a9
erkan.esmer <erkanesmer@gmail.com>
2026-04-27 10:03:16 +0300

    ScripTool directory created

---
+++ ScripTools/rdp-connect.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ----------------------------------------
+# Name : rdp-connect.sh
+# Usage: bash rdp-connect.sh 192.168.2.11 administrator testdomain
+# Extra: man xfreerdp
+# ----------------------------------------
+
+SERVER="$1"
+USERNAME="$2"
+DOMAIN="$3"
+
+xfreerdp /v:$SERVER /u:$USERNAME /d:$DOMAIN \
+	 /dynamic-resolution \
+         +clipboard \
+         /cert:tofu
+
