f4878fa3a66db0c2e51e9cffd73b0f26a0a9b347
erkan.esmer <erkanesmer@gmail.com>
2026-04-27 10:04:58 +0300

    ScripTool/rdp-connect.sh edited

--- ScripTools/rdp-connect.sh
+++ ScripTools/rdp-connect.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # ----------------------------------------
-# Name : rdp-connect.sh
-# Usage: bash rdp-connect.sh 192.168.2.11 administrator testdomain
-# Extra: man xfreerdp
+# Name   : rdp-connect.sh
+# Usage  : bash rdp-connect.sh 192.168.2.11 administrator testdomain
+# Extra  : man xfreerdp
+# Package: xfreerdp - apt -y install xfreerdp
 # ----------------------------------------
 
 SERVER="$1"
