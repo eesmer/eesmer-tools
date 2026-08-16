4ce25ac55f7e46a8c67702f45c9383e111356011
Erkan Esmer <erkanesmer@gmail.com>
2026-08-16 10:05:47 +0300

    Modify rdp-connect.sh to use freerdp3
    Updated script to use freerdp3 and added installation instructions for different session types.

--- ScripTools/rdp-connect.sh
+++ ScripTools/rdp-connect.sh
@@ -4,14 +4,17 @@
 # Name   : rdp-connect.sh
 # Usage  : bash rdp-connect.sh 192.168.2.11 administrator testdomain
 # Extra  : man xfreerdp
-# Package: xfreerdp - apt -y install xfreerdp
+# Install: echo "$XDG_SESSION_TYPE" / loginctl show-session "$XDG_SESSION_ID" -p Type
+           x11     -> apt install freerdp3-x11
+		   wayland -> apt install freerdp3-wayland
+# Tested : Debian13
 # ----------------------------------------
 
 SERVER="$1"
 USERNAME="$2"
 DOMAIN="$3"
 
-xfreerdp /v:$SERVER /u:$USERNAME /d:$DOMAIN \
+xfreerdp3 /v:$SERVER /u:$USERNAME /d:$DOMAIN \
 	 /dynamic-resolution \
          +clipboard \
          /cert:tofu
